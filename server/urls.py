6a7e0db231e10de86d14038db14cfb5148ca9410
igor531205 <igor531205@gmail.com>
2023-05-09 18:25:36 +0300

    start project

--- server/urls.py
+++ server/urls.py
@@ -1,7 +1,8 @@
-"""furry_tail URL Configuration
+"""
+URL configuration for server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from store.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/apps.py
+++ store/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ShowcaseConfig(AppConfig):
+class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'showcase'
+    name = 'store'
