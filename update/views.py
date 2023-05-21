cacc11da11c95016a4e3d0ead18bd6d27d3fb903
igor531205 <igor531205@gmail.com>
2023-05-21 20:54:55 +0300

    update catalogue

--- update/views.py
+++ update/views.py
@@ -10,4 +10,7 @@ class UpdateView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         system('git -C ~/furry_tail/ pull origin main')
+        system('sudo systemctl restart nginx')
+        system('sudo systemctl restart gunicorn')
+        system('sudo systemctl restart celery')
         return super().get_redirect_url(*args, **kwargs)
